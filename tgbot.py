3d3b4fff13a5008035967778b6e3b3b2d35bfe60
Masterofponchik <danyalungu@gmail.com>
2026-02-21 15:55:46 +0200

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -11,7 +11,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, FSInputFil
 
 OWNER_ID = 141230163
 TOKEN = "8584190260:AAFpwFdaHFBroUeBmS6EszIh-kDAGAMppRE"
-SUPPORT_USERNAME = "@@g_gggggg_g"
+SUPPORT_USERNAME = "@g_gggggg_g"
 SUPPORT_URL = "https://t.me/BreakLegenda"
 CHANNEL_ID = -1003830241596
 CHANNEL_URL = "https://t.me/BreakLegenda"
@@ -269,4 +269,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
